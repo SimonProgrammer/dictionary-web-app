59cd7e956a284c1c0be8f6b85e3198e1755571fe
Simon Alberto Lopez Higuera <lopezhsimon@gmail.com>
2024-09-28 15:04:54 -0500

    ✅ feat: prettier and eslint configured

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+      '@/components': path.resolve(__dirname, './src/components'),
     },
   },
   plugins: [react()],
